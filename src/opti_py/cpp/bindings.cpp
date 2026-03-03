5b49f608db838c2b506e4b6ea72fb344ab9e42b9
Alex Buckley <alexmeb81@outlook.com>
2026-03-02 20:33:45 -0800

    Implemented bindings for Flowshop cpp class with shared python memory for job times matrix

--- src/opti_py/cpp/bindings.cpp
+++ src/opti_py/cpp/bindings.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 
+#include "FlowShop/FlowShop.h"
 #include "Optimizer/DifferentialEvolution.h"
 #include "Optimizer/ParticleSwarm.h"
 #include "ExperimentConfig.h"
@@ -10,6 +11,93 @@ namespace py = pybind11;
 
 
 PYBIND11_MODULE(_opti_py, m) {
+
+    py::class_<FlowShop>(m, "FlowShop")
+
+        // default
+        .def(py::init<>())
+
+        // construct from dimensions
+        .def(py::init<size_t, size_t>(),
+            py::arg("num_jobs"),
+            py::arg("num_machines"))
+
+        // construct from numpy array
+        .def(py::init([](
+                py::array_t<uint64_t,
+                            py::array::c_style | py::array::forcecast> arr)
+        {
+            auto buf = arr.request();
+
+            if (buf.ndim != 2)
+                throw std::runtime_error("jobs must be 2D");
+
+            size_t num_jobs = buf.shape[0];
+            size_t num_machines = buf.shape[1];
+
+            std::vector<uint64_t> data(num_jobs * num_machines);
+
+            std::memcpy(data.data(),
+                        buf.ptr,
+                        data.size() * sizeof(uint64_t));
+
+            return FlowShop(num_jobs, num_machines, std::move(data));
+        }),
+        py::arg("jobs"))
+
+        // zero-copy NumPy view
+        .def_property("jobs",
+
+            [](FlowShop& self) {
+                return py::array_t<uint64_t>(
+                    { self.num_jobs(), self.num_machines() },
+                    { self.num_machines() * sizeof(uint64_t),
+                    sizeof(uint64_t) },
+                    self.data(),
+                    py::cast(&self)
+                );
+            },
+
+            [](FlowShop& self,
+            py::array_t<uint64_t,
+                        py::array::c_style | py::array::forcecast> arr)
+            {
+                auto buf = arr.request();
+
+                if (buf.ndim != 2)
+                    throw std::runtime_error("jobs must be 2D");
+
+                size_t num_jobs = buf.shape[0];
+                size_t num_machines = buf.shape[1];
+
+                std::vector<uint64_t> new_data(num_jobs * num_machines);
+
+                std::memcpy(new_data.data(),
+                            buf.ptr,
+                            new_data.size() * sizeof(uint64_t));
+
+                self.set_jobs(num_jobs,
+                            num_machines,
+                            std::move(new_data));
+            }
+        )
+        .def_property_readonly("num_jobs",
+            &FlowShop::num_jobs)
+        .def_property_readonly("num_machines",
+            &FlowShop::num_machines);
+
+        .def("runNEH",
+            &FlowShop::runNEH,
+            py::arg("blocking") = false,
+            "Run the NEH heuristic and return a FlowShopResult");
+
+
+    py::class_<FlowShopResult>(m, "FlowShopResult")
+        .def_readwrite("sequence", &FlowShopResult::sequence)
+        .def_readwrite("makespan", &FlowShopResult::makespan)
+        .def_readwrite("completionTimes", &FlowShopResult::completionTimes);
+
+
 
     py::class_<ExperimentConfig>(m, "ExperimentConfig")
         .def(py::init<
