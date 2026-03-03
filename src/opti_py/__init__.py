59a7ff273df13dbfe24a2beebf5fe760cf966769
Alex Buckley <alexmeb81@outlook.com>
2026-03-02 20:37:00 -0800

    Made FlowShop and results classes as directly importble from Python

--- src/opti_py/__init__.py
+++ src/opti_py/__init__.py
@@ -1,6 +1,12 @@
 
 # Expose Optimizer algorithms
-from ._opti_py import DifferentialEvolution, ParticleSwarm, ExperimentConfig
+from ._opti_py import (
+    DifferentialEvolution,
+    ParticleSwarm,
+    ExperimentConfig,
+    FlowShop,
+    FlowShopResults,
+)
 
 # Expose problem ID mapping Enum
 from .problem import Problem
