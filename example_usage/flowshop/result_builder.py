b59e67ac323a86fdd1d76843985c772fed2ac517
Alex Buckley <alexmeb81@outlook.com>
2026-03-12 18:51:03 -0700

    Added support for verbose printing output

--- example_usage/flowshop/result_builder.py
+++ example_usage/flowshop/result_builder.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from .plot_builder import build_plots
 
 
-def build_results(data: pd.DataFrame, output_dir: Path) -> None:
+def build_results(data: pd.DataFrame, output_dir: Path, verbose: bool = False) -> None:
     # Create dir to store plots
     plot_dir: Path = output_dir / 'plots'
     plot_dir.mkdir(parents=True, exist_ok=True)
 
-    build_plots(data, plot_dir)
+    build_plots(data, plot_dir, verbose)
